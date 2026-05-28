5ad13400e242ec52e55b3fe22b8e2b0e0d07bee6
Juan Diaz <juanignaciodiazpuig998@gmail.com>
2026-05-27 21:23:26 -0300

    chore: add lint and format github actions and configs

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["client/src/**/*.ts", "server/src/**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  prettier,
+];
