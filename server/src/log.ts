feaf55a26a70189351a978ed8cba09cd536a7a6f
Juan Diaz <juanignaciodiazpuig998@gmail.com>
2026-06-17 20:08:58 -0300

    fix: vscode dependency missing on build

--- server/src/log.ts
+++ server/src/log.ts
@@ -1,6 +1,10 @@
 import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
 
-const log = fs.createWriteStream("/home/juan/ort-proyectos/shell-language-server/lsp.log");
+const log = fs.createWriteStream(path.join(os.tmpdir(), "shell-lsp.log"), { flags: "a" });
+
+log.on("error", () => {});
 
 export default {
   write: (message: object | unknown) => {
